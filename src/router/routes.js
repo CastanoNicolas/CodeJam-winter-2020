3fa90a705452017b9734687f21b03e2b5e56d87b
Nicolas Castano <castano.nicolas.nc@gmail.com>
2020-02-07 19:34:50 -0400

    ajout de pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,11 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '/ShoppingList', component: () => import('pages/ShoppingList.vue') },
+      { path: '/StockList', component: () => import('pages/StockList.vue') },
+      { path: '/IngredientList', component: () => import('pages/IngredientList.vue') },
+      { path: '/RecipeList', component: () => import('pages/RecipeList.vue') },
+      { path: '/IngredientModification', component: () => import('pages/IngredientModification.vue') },
     ]
   }
 ]
