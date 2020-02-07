342dddadf51245650746bc10e026c2af59905a4f
Nicolas Castano <castano.nicolas.nc@gmail.com>
2020-02-07 19:36:40 -0400

    small fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ const routes = [
       { path: '/StockList', component: () => import('pages/StockList.vue') },
       { path: '/IngredientList', component: () => import('pages/IngredientList.vue') },
       { path: '/RecipeList', component: () => import('pages/RecipeList.vue') },
-      { path: '/IngredientModification', component: () => import('pages/IngredientModification.vue') },
+      { path: '/IngredientModification', component: () => import('pages/IngredientModification.vue') }
     ]
   }
 ]
