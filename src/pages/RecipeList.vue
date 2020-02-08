ae9be980832c26a17fb3a77dd3b71ac161e124aa
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-08 16:57:40 +0100

    ajout methode de check nombre ingredient

--- src/pages/RecipeList.vue
+++ src/pages/RecipeList.vue
@@ -32,14 +32,27 @@ export default {
       let newRecipe = new Recipe(ingredientList, quantityList, description, categories)
       this.$store.commit('addRecipeToRecipeList', newRecipe)
     },
-    addIngredientFromRecipeToShoppingList () {
-
+    addIngredientFromRecipeToShoppingList (recipe) {
+      recipe.ingredientList.forEach(function (ingredient, index, array) {
+        this.addIngredientToShoppingList(ingredient, recipe.quantityList[index])
+      })
     },
-    removeIngredientFromRecipeFromStockList () {
+    removeIngredientFromRecipeFromStockList (recipe) {
+      recipe.ingredientList.forEach(
+        (ingredient, index, array) => {
+          if (this.enoughIngredient(ingredient)) {
 
+          }
+        }
+      )
     },
-    addRecipeToStockList () {
-
+    addRecipeToStockList (recipe) {
+      // with expiry date
+    },
+    enoughIngredient (ingredient, quantity) {
+      return this.$store.getters.mainModule.getStockList().find((elem) => {
+        return elem.name === ingredient.name
+      }).quantity >= quantity
     }
   },
   mixins: [listManagerMixin]
