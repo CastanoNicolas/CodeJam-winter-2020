9b083f24ecd907e9c8e1d92560c1a6fe21717dee
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-08 15:37:52 +0100

    RecipeList Copier/coller depuis shopping list
    juste le setup

---
+++ src/pages/RecipeList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="fixed-center text-center">
+    RecipeList {{test}}
+    <q-btn color="white" text-color="black" label="Standard" @click="testmet()" />
+  </div>
+</template>
+
+<script>
+import { listManagerMixin } from '../mixins/listManagerMixin'
+
+export default {
+  name: 'RecipeListList',
+  data () {
+    return {
+      a: { blub: 'A', blib: '' },
+      b: { blub: 'B', blib: 'Z' },
+      c: []
+    }
+  },
+  computed: {
+    test () {
+      return { blub: 'A', blib: '', ...this.b }
+    },
+    ingredientList () {
+      return this.$store.getters.mainModule.getShoppingList
+    }
+  },
+  methods: {
+    removeIngredientFromShoppingList (ingredient, quantity) {
+      this.$store.commit('removeIngredientFromShoppingList', { ingredient, quantity })
+    },
+    testmet () {
+
+    }
+  },
+  mixins: [listManagerMixin]
+
+}
+</script>
