a34a9faa47a2e644863398ab84638ba0c5966ddd
Nicolas Castano <castano.nicolas.nc@gmail.com>
2020-02-07 22:51:48 -0400

    page stocklist

---
+++ src/pages/StockList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fixed-center text-center">
+    ShoppingList {{test}}
+    <q-btn color="white" text-color="black" label="Standard" @click="testmet()" />
+  </div>
+</template>
+
+<script>
+import { listManagerMixin } from '../mixins/listManagerMixin'
+
+export default {
+  name: 'StockList',
+  computed: {
+
+    ingredientList () {
+      return this.$store.getters.mainModule.getShoppingList
+    }
+  },
+  methods: {
+    removeIngredientFromStockList (ingredient, quantity) {
+      this.$store.commit('removeIngredientFromStockList', { ingredient, quantity })
+    }
+  },
+  mixins: [listManagerMixin]
+}
+</script>
