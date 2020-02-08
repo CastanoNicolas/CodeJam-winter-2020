e2bc1c6484cde10aeade5a9b698cbf189e4435af
Nicolas Castano <castano.nicolas.nc@gmail.com>
2020-02-08 12:21:40 -0400

    fixed ingredientList

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/StockList.vue') },
+      { path: '', component: () => import('pages/IngredientList.vue') },
       { path: '/StockList', component: () => import('pages/ShoppingList.vue') },
-      { path: '/IngredientList', component: () => import('pages/IngredientList.vue') },
+      { path: '/IngredientList', component: () => import('pages/StockList.vue') },
       { path: '/RecipeList', component: () => import('pages/RecipeList.vue') },
       { path: '/IngredientModification', component: () => import('pages/IngredientModification.vue') }
     ]
