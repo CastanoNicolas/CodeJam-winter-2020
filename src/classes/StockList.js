1f3ad07095925113c6a1b280cbce59d185effd05
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-08 23:30:58 +0100

    Ajout et suprression d'ingredient ok !

--- src/classes/StockList.js
+++ src/classes/StockList.js
@@ -25,12 +25,7 @@ export class StockList {
       this.ingredientList[ingredient.name] = []
     }
 
-    let exist = this.ingredientExist(ingredient)
-    if (exist) {
-      exist.quantity += quantity
-    } else {
-      this.ingredientList[ingredient.name].push({ ingredient: { ...ingredient, expiryDate: Date.now + ingredient.dayBeforeStale }, quantity: quantity })
-    }
+    this.ingredientList[ingredient.name].push({ ingredient: { ...ingredient, expiryDate: Date.now() + ingredient.dayBeforeStale }, quantity: quantity })
   }
   removeIngredient (ingredient, quantity) {
     // if (this.ingredientList[ingredient.name].length <= quantity) {
@@ -41,10 +36,15 @@ export class StockList {
     //   }
     // }
     let exist = this.ingredientExist(ingredient)
-    if (exist) {
-      exist.quantity -= quantity
-      if (exist.quantity < 0) {
-        delete this.ingredientList[exist.name]
+    if (exist !== undefined) {
+      this.ingredientList[ingredient.name][exist].quantity -= quantity
+      if (this.ingredientList[ingredient.name][exist].quantity <= 0) {
+        let rest = -this.ingredientList[ingredient.name][exist].quantity
+        delete this.ingredientList[ingredient.name][exist]
+        let exist2 = this.ingredientExist(ingredient)
+        if (exist2 !== undefined && exist2 !== -1 && rest !== 0) {
+          this.removeIngredient(ingredient, rest)
+        }
       }
     }
   }
@@ -75,25 +75,29 @@ export class StockList {
     }
   }
   ingredientExist (ingredient) {
-    return this.ingredientList[ingredient.name].find((elem) => {
+    return this.ingredientList[ingredient.name].findIndex((elem) => {
       // remplacer par for ... in
+      if (!elem) {
+        return false
+      }
+      elem = elem.ingredient
       if (elem.name === ingredient.name &&
          elem.dayBeforeStale === ingredient.dayBeforeStale &&
-         elem.expiryDate === ingredient.expiryDate &&
          elem.unity === ingredient.unity &&
          elem.categories === ingredient.categories) {
+        console.log('ingredient EXIST')
         return true
       } else {
+        console.log('ingredient NOT exist')
         return false
       }
     })
   }
   recipeExist (recipe) {
-    return this.recipeList.find((elem) => {
+    return this.recipeList.findIndex((elem) => {
       // remplacer par for ... in
       if (elem.name === recipe.name &&
          elem.dayBeforeStale === recipe.dayBeforeStale &&
-         elem.expiryDate === recipe.expiryDate &&
          elem.ingredientList === recipe.ingredientList &&
          elem.quantityList === recipe.quantityList &&
          elem.description === recipe.description &&
