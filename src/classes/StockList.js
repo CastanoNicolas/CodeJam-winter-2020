2383ce02894ca96c64410bea403cd722dfcd9f0a
Axelexa <axel.zerbin@isen.yncrea.fr>
2020-02-08 16:31:01 +0100

    ajout des fonction pour gerer recette dans les stocks

--- src/classes/StockList.js
+++ src/classes/StockList.js
@@ -2,10 +2,14 @@
 export class StockList {
   constructor () {
     this.ingredientList = []
+    this.recipeList = []
   }
   addIngredient (ingredient, quantity) {
     for (let index = 0; index < quantity; index++) {
       // let object = new Ingredient(ingredient.name, ingredient.dayBeforeStale, Date.now() + ingredient.dayBeforeStale, ingredient.unity, ingredient.categories)
+      /*
+      JE CROIS QUE CA MARCHE PAS ICI !!!
+      */
       if (typeof this.ingredientList[ingredient.name] === 'undefined') {
         this.ingredientList[ingredient.name] = []
       }
@@ -15,6 +19,17 @@ export class StockList {
   removeIngredient (ingredient, quantity) {
     for (let index = 0; index < quantity; index++) {
       this.ingredientList[ingredient.name].shift()
+    }
+  }
+  addRecipe (recipe, quantity) {
+    if (typeof this.ingredientList[recipe.name] === 'undefined') {
+      this.recipeList[recipe.name] = []
+    }
+    this.recipeList[recipe.name].push({ ...recipe, expiryDate: Date.now() + recipe.dayBeforeStale })
+  }
+  removeRecipe (recipe, quantity) {
+    for (let index = 0; index < quantity; index++) {
+      this.recipeList[recipe.name].shift()
     }
   }
 }
