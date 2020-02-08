0ea92d4962cdf8a34b0bd2f827bdd112020cb439
Liehko <juliette.laplante.jl@gmail.com>
2020-02-07 23:24:02 -0500

    wip

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-footer>
+    <q-tabs>
+      <q-route-tab class="tab" icon="kitchen" to="/StockList">Stock</q-route-tab>
+      <q-route-tab class="tab" icon="local_grocery_store" to="/ShoppingList">Shopping</q-route-tab>
+      <q-route-tab class="tab" icon="menu_book" to="/RecipeList">Recipes</q-route-tab>
+      <q-route-tab class="tab" icon="fas fa-carrot" to="/IngredientList">Ingredients</q-route-tab>
+    </q-tabs>
+  </q-footer>
+</template>
+
+<script>
+export default {
+  name: 'NavigationMenu',
+  props: {
+    activeTab: String
+  }
+}
+</script>
+
+<style>
+.tab {
+  font-size: 12px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+</style>
---
+++ src/pages/StockList.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page>
+    <q-input class="q-pa-md" outlined v-model="text" label="Label">
+      <template v-slot:append>
+        <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
+        <q-icon name="search" />
+      </template>
+
+      <template v-slot:hint>
+        Field hint
+      </template>
+    </q-input>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'StockList'
+}
+</script>
+
+<style>
+</style>
