3d51fb49c8ab1e3c2cac6cd1f070bfadc510f4c2
Yoann HAFFNER <yoyofrom68@gmail.com>
2020-02-08 22:44:24 -0500

    Changed icon add ingredient from 'archive' to 'add' icone

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,8 +47,12 @@ module.exports = function (ctx) {
       //            (not treeshaking Quasar; biggest bundle size; convenient)
       all: 'auto',
 
-      components: [],
-      directives: [],
+      components: [
+        'QDialog'
+      ],
+      directives: [
+        'ClosePopup'
+      ],
 
       // Quasar plugins
       plugins: []
--- src/components/IngredientItem.vue
+++ src/components/IngredientItem.vue
@@ -1,6 +1,6 @@
 <template>
   <q-item class="q-mb-md">
-    <q-item-section class="q-pl-md column" to="/RecipeView">
+    <q-item-section class="q-pl-md column">
       <q-item-label class="col text-weight-medium">{{ingredientItem.name}}</q-item-label>
       <div class="q-pt-xs q-gutter-md col">
         <q-badge
@@ -11,6 +11,7 @@
         >{{cat}}</q-badge>
       </div>
     </q-item-section>
+      <q-btn flat round icon="add" />
       <q-btn flat round icon="edit" />
       <q-btn flat round icon="delete" />
   </q-item>
